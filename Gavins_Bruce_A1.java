93a23cb8aaf54e856997f04da9e9cafd141b8ebc
brucegav <brucegavins@gmail.com>
2025-06-09 16:17:52 -0400

    Initial commit- print println demo

---
+++ Gavins_Bruce_A1.java
@@ -0,0 +1,32 @@
+//Gavins_Bruce_A1.java
+//**Countdown.java*/
+//** Demonstrates the difference between print and println*/
+
+//**Bruce Gavins*/
+//**MSI-5030*/
+//**Assignment 1*/
+
+
+//***********************************************************************************
+
+public class Gavins_Bruce_A1
+{
+	//** prints multiple lines of output summarizing the plot of Apollo 13.
+
+	public static void main(String[] args)
+	{
+	System.out.print("Three...");
+	System.out.print("Two...");
+	System.out.print("One...");
+	System.out.print("Zero...");  //these all appear on one line with the follwing print statement
+        
+
+	System.out.println("Liftoff!"); //appears on its own output line with above
+
+	System.out.println("...an hour passes in our film..."); //appears on its own output line
+	System.out.println("Gary Sinise may have the measles!!"); //appears on its own output line
+	System.out.println("Kevin Bacon doesn't how to stir on O2 tank"); //appears on its own output line
+
+	System.out.println("Houston, we have a problem."); //appears on its own output line
+	}
+}
